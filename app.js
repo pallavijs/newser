0811befee6c14115b8961555e843925025023754
Pallavi Panwar <terminal.pallavi@gmail.com>
2023-05-16 13:48:47 +0530

    comment: comment the unused code

--- app.js
+++ app.js
@@ -56,23 +56,23 @@ class="card bg-green-200 flex flex-col justify-center rounded-lg m-2 sm:w-58 w-9
 
 
 
-const fetchNewsFromApi = async()=>{
+// const fetchNewsFromApi = async()=>{
 
 
-    const url = 'https://newscatcher.p.rapidapi.com/v1/search_enterprise?q=Elon%20Musk&lang=en&sort_by=relevancy&page=1&media=True';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'a8050b5b57mshbb1358d56cb53a8p1e67c1jsnd6a2647f4f70',
-		'X-RapidAPI-Host': 'newscatcher.p.rapidapi.com'
-	}
-};
+//     const url = 'https://newscatcher.p.rapidapi.com/v1/search_enterprise?q=Elon%20Musk&lang=en&sort_by=relevancy&page=1&media=True';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'a8050b5b57mshbb1358d56cb53a8p1e67c1jsnd6a2647f4f70',
+// 		'X-RapidAPI-Host': 'newscatcher.p.rapidapi.com'
+// 	}
+// };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-}
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+// }
